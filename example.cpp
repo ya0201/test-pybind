2db911f15f3cbbf32fe4e9a0c084ffc6a33ad68f
ya0201 <you@example.com>
2018-10-09 15:25:38 +0900

    Add vector<double>(C++) -> list(python) code

--- example.cpp
+++ example.cpp
@@ -1,4 +1,6 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <cstdlib>
 extern "C" {
 #include "hoge.h"
 }
@@ -14,9 +16,15 @@ void reset() {
   global_num = 0;
 }
 
+std::vector<double> getDoubleVec() {
+  std::vector<double> v = {(double)rand()/RAND_MAX, (double)rand()/RAND_MAX, (double)rand()/RAND_MAX};
+  return v;
+}
+
 PYBIND11_MODULE(example, m) {
   m.doc() = "pybind11 example plugin";
 
   m.def("test", &test, "A function returns number of call");
   m.def("reset", &reset, "A function resets global number");
+  m.def("getDoubleVec", &getDoubleVec, "A function returns vector<double>");
 }
